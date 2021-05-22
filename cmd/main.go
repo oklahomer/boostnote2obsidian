bfd1c05b6c6ae7ec917dc3264b060489fcb0bde2
Oklahomer <hagiwara.go@gmail.com>
2021-05-22 10:29:54 +0900

    Create directory per generated month

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,7 @@ import (
 	"path"
 	"path/filepath"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -17,28 +18,31 @@ func main() {
 	to := flag.String("to", "", "Directory path to export converted files")
 	flag.Parse()
 
+	// Check required params
 	if *from == "" || *to == "" {
 		log.Fatal("./path/main.go -from=/path/to/boostnote/storage -to=/path/to/target")
 	}
 
-	err := os.MkdirAll(*to, os.ModePerm)
-	if err != nil {
-		log.Fatal("Failed to setup target directory")
-	}
-
-	err = filepath.Walk(*from, func(filePath string, info os.FileInfo, e error) error {
+	err := filepath.Walk(*from, func(filePath string, info os.FileInfo, e error) error {
 		if !strings.HasSuffix(filePath, ".json") {
 			// Skip an irrelevant file
 			return nil
 		}
 
+		// Read note file
 		note, e := boostnote.ReadNote(filePath)
 		if e != nil {
 			log.Printf("Failed to read note %s: %s\n", filePath, e.Error())
 			return nil
 		}
 
-		targetPath := path.Join(*to, genFileName(note))
+		// Prepare export destination
+		targetPath := genFilePath(*to, note)
+		dir := path.Dir(targetPath)
+		err := os.MkdirAll(dir, os.ModePerm)
+		if err != nil {
+			return fmt.Errorf("failed to create directory: %w", e)
+		}
 		file, e := os.Create(targetPath)
 		if e != nil {
 			return fmt.Errorf("failed to create file: %w", e)
@@ -47,6 +51,7 @@ func main() {
 			file.Close()
 		}()
 
+		// Export content
 		_, e = file.WriteString(note.Content)
 		if e != nil {
 			return fmt.Errorf("failed to write %s: %w", targetPath, e)
@@ -54,7 +59,7 @@ func main() {
 		return nil
 	})
 	if err != nil {
-		log.Fatal("%s", err.Error())
+		log.Fatalf("Failed to walk through the directory: %s", err.Error())
 	}
 }
 
@@ -62,13 +67,12 @@ var safeTitle = regexp.MustCompile(`[^[:alnum:]-.]`)
 var sepChars = regexp.MustCompile(`[ &_=+:]`)
 var dashChars = regexp.MustCompile(`[\-]+`)
 
-func genFileName(note *boostnote.Note) string {
+func genFilePath(baseDir string, note *boostnote.Note) string {
 	name := strings.ToLower(note.Title)
 	name = strings.TrimSpace(name)
 	name = sepChars.ReplaceAllString(name, "-")
 	name = safeTitle.ReplaceAllString(name, "")  // Non ascii to empty string
 	name = dashChars.ReplaceAllString(name, "-") // Multiple dashes to single
-
 	if name == "" {
 		name = fmt.Sprintf(
 			"%d-%02d-%02d-%02d-%02d",
@@ -78,6 +82,7 @@ func genFileName(note *boostnote.Note) string {
 			note.CreatedAt.Hour(),
 			note.CreatedAt.Minute())
 	}
+	name = fmt.Sprintf("%s.md", name)
 
-	return fmt.Sprintf("%s.md", name)
+	return path.Join(baseDir, strconv.Itoa(note.CreatedAt.Year()), fmt.Sprintf("%02d", note.CreatedAt.Month()), name)
 }
